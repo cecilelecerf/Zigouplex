5cf3708994656e6d5f00054954d77fb579c78adf
cecilelecerf <c.lecerf.cecile@gmail.com>
2024-12-15 23:58:17 +0100

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.js
+
+module.exports = {
+  siteUrl: 'https://www.zigouplex.shop', // Remplace par l'URL de ton site
+  generateRobotsTxt: true, // Si tu veux générer un fichier robots.txt
+  sitemapSize: 7000, // Nombre maximum d'URL par fichier sitemap (optionnel)
+  changefreq: 'daily', // Fréquence de mise à jour des URLs
+  priority: 0.7, // Priorité de la page
+};
